7917dcc762a6f240bd3d65556cc5c41754d96cd8
Vaishnavraut10 <vaishnavraut2010@gmail.com>
2025-02-09 18:44:02 +0530

    Updates

---
+++ precache-manifest.401212458ccd97291f25f2e705a19e0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c301fbc51ca4b3872761ce4a50f532",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "a721f900a13280bf7e2c",
+    "url": "/react-app/static/css/main.fcfdd5b2.chunk.css"
+  },
+  {
+    "revision": "7ea5ed0c2d506075ddf2",
+    "url": "/react-app/static/js/2.a005a0f5.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/react-app/static/js/2.a005a0f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b06ef704ca9acd9d6e3",
+    "url": "/react-app/static/js/3.ec8cbe19.chunk.js"
+  },
+  {
+    "revision": "a721f900a13280bf7e2c",
+    "url": "/react-app/static/js/main.66e28d7e.chunk.js"
+  },
+  {
+    "revision": "d3dfa661261e5c57e82e",
+    "url": "/react-app/static/js/runtime-main.92e6fd02.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-app/static/media/logo.06e73328.svg"
+  }
+]);
